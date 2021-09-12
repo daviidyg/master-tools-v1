3f33511d9543cd6e3270c22c8478bf4c0615da17
David <ozirus213@gmail.com>
2021-09-12 23:19:54 +0200

    Creating a JWT with nodejs

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect} from "react";
-import {CombatLogComponent} from "@seed/combat-log/combat-log-component";
 import {RoleCardInfo} from "@seed/core/models/card-info";
 import {modelsSet} from "@seed/core/store/slices/app.slice";
 import {useDispatch} from "react-redux";
+
 
 
 export const AppComponent = () => {
@@ -12,28 +12,28 @@ export const AppComponent = () => {
     useEffect(() => {
         dispatch(
             modelsSet([
-            {
-                id: 1,
-                name: "Ozirus",
-                role: RoleCardInfo.HEALER,
-                ca: 3,
-                hp: 30,
-                currentHp: 10
-            },
-            {
-                id: 2,
-                name: "Ozirus",
-                role: RoleCardInfo.MONSTER,
-                ca: 3,
-                hp: 500,
-                currentHp: 250
-            }])
+                {
+                    id: 1,
+                    name: "Ozirus",
+                    role: RoleCardInfo.HEALER,
+                    ca: 3,
+                    hp: 30,
+                    currentHp: 10
+                },
+                {
+                    id: 2,
+                    name: "Ozirus",
+                    role: RoleCardInfo.MONSTER,
+                    ca: 3,
+                    hp: 500,
+                    currentHp: 250
+                }])
         );
     }, []);
 
     return (
         <>
-            <CombatLogComponent />
+
         </>
     );
 }
