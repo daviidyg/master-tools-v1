c8a7ad2c92b2077178644326f84dfc7fa163bf48
David <ozirus213@gmail.com>
2022-02-01 19:07:14 +0100

    apirest in nodejs

--- api/src/dbConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    url: 'mongodb://172.17.0.2:27017/ApiRest',
-    user: 'admin',
-    pwd: 'admin'
-}
--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,39 +1,11 @@
-import React, {useEffect} from "react";
-import {RoleCardInfo} from "@seed/core/models/card-info";
-import {modelsSet} from "@seed/core/store/slices/app.slice";
-import {useDispatch} from "react-redux";
-
+import React from "react";
 
 
 export const AppComponent = () => {
 
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(
-            modelsSet([
-                {
-                    id: 1,
-                    name: "Ozirus",
-                    role: RoleCardInfo.HEALER,
-                    ca: 3,
-                    hp: 30,
-                    currentHp: 10
-                },
-                {
-                    id: 2,
-                    name: "Ozirus",
-                    role: RoleCardInfo.MONSTER,
-                    ca: 3,
-                    hp: 500,
-                    currentHp: 250
-                }])
-        );
-    }, []);
-
     return (
         <>
-
+            <p>olla</p>
         </>
     );
 }
