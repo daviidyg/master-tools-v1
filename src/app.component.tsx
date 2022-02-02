1a1d765e84d3cdbf7dbe4bf29ca0c309bd0dab61
David <ozirus213@gmail.com>
2022-02-02 23:16:47 +0100

    added clock

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {ClockComponent} from "@seed/components/clock-component/clock-component";
 
 
 export const AppComponent = () => {
@@ -6,6 +7,7 @@ export const AppComponent = () => {
     return (
         <>
             <img src="images/bg-image.jpeg" style={{height: "100vh", width: "100%", overflowY: "auto"}}/>
-        </>
+            <ClockComponent/>
+       </>
     );
 }
---
+++ src/components/clock-component/clock-component.tsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react";
+
+export const ClockComponent = () => {
+
+    const [time, setTime] = useState(new Date());
+    const refreshClock = () => setTime(new Date());
+
+    useEffect(() => {
+        const clockInterval = setInterval(refreshClock, 1000);
+        return () => {
+            clearInterval(clockInterval);
+        }
+    }, []);
+    
+
+    return (
+        <h1 className="text-light position-absolute " style={{right: "5%", top: "10%", fontSize: "10vh"}}>{time.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})}</h1>
+    );
+}
